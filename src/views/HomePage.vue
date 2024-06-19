4efd01606606c30fceba34f34eabf70b71379420
Elkin Rojas <rojaselkiniser@gmail.com>
2024-06-19 07:34:02 -0500

    Implemento servicio para consulta de géneros en HomePage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,23 @@
 <template>
   <header-app :title="title"/>
-  <ion-title size="large">Home</ion-title>
+  <ion-title size="large">
+    Géneros
+  </ion-title>
+  <ion-row>
+    <ion-col v-for="genre in genres">
+      <ion-card>
+        <ion-card-content>
+          {{ genre.name }}
+        </ion-card-content>
+      </ion-card>
+    </ion-col>
+  </ion-row>
 </template>
 
 <script lang="ts">
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import HeaderApp from '@/components/HeaderApp.vue'
+import MovieServices from '@/services/MovieServices'
 export default {
   name: 'HomePage',
   components: {
@@ -13,8 +25,13 @@ export default {
   },
   data(){
     return {
-      title: 'Home'
+      title: 'Home',
+      genres: []
     }
+  },
+  async created() {
+    this.genres = await MovieServices.getGenrers()
+    console.log(this.genres)
   }
 }
 </script>
