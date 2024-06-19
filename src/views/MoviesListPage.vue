90bf4bede3e36957c010a14e0a47f3bea474b781
Elkin Rojas <rojaselkiniser@gmail.com>
2024-06-19 07:57:43 -0500

    Implemento servicio de peliculas por genero

--- src/views/MoviesListPage.vue
+++ src/views/MoviesListPage.vue
@@ -1,5 +1,38 @@
 <template>
+    <header-app title="Listado"/>
     <ion-title size="large">Listado</ion-title>
 </template>
-<script></script>
+<script lang="ts">
+import HeaderApp from '@/components/HeaderApp.vue'
+import { useRoute } from 'vue-router'
+import MovieServices from '@/services/MovieServices'
+import {IonTitle} from '@ionic/vue'
+export default {
+    name: 'MoviesListPage',
+    components: {
+        HeaderApp,
+        IonTitle
+    },
+    data() {
+        return {
+            genre_id : 1,
+            page: 1,
+            movies: []
+        }
+    },
+    async created(){
+        const route = useRoute()
+        this.genre_id = route.params.genre_id
+        await this.getMovies()
+    },
+    methods: {
+        async getMovies(){
+            this.movies = await MovieServices.getMoviesFilteredByGenre(this.genre_id,this.page)
+            console.log('movies', this.movies)
+        }
+    }
+    
+}
+
+</script>
 <style></style>
